53cded6938f40733ada8da58dd4c4aa031100227
Alan Yasuhiro <alannishida1997@gmail.com>
2020-01-14 21:22:03 -0300

    Updates

---
+++ precache-manifest.6d561fd6fed1ab389d378f1f896294e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdff052b0ae1ed3af0c0d479df2c8097",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ce33bef588d95c561d9c",
+    "url": "/monster-rolodex/static/css/main.5bb0f6f2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce33bef588d95c561d9c",
+    "url": "/monster-rolodex/static/js/main.95c47c14.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
